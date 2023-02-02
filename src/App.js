bb6b496b4cc44d1cea465bfa231800cb145afadb
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2023-02-02 10:47:32 -0500

    styled components demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Header = styled.header`
+  background-color: lightblue;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  color: white;
+  font-size: 24px;
+`;
+const Main = styled.main`
+  display: flex;
+  width: 100%;
+  height: calc(100vh - 120px);
+`;
+
+const Section = styled.section`
+  background-color: white;
+  width: 80%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  padding: 20px;
+`;
+
+const Aside = styled.aside`
+  background-color: lightgray;
+  width: 20%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 10px;
+  padding: 20px;
+`;
+
+const Footer = styled.footer`
+  background-color: lightblue;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Header>
+          <Title>Welcome to my homepage</Title>
+        </Header>
+        <Main>
+          <Section>
+            <p>This is the main content</p>
+          </Section>
+          <Aside>
+            <p>Side content</p>
+          </Aside>
+        </Main>
+        <Footer>This is the footer</Footer>
+      </Container>
     </div>
   );
 }
